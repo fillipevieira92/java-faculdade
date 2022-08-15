fe299b16806aac7b1c3d7b896817511664332881
fillipevieira92 <fillipevieira92@gmail.com>
2022-08-15 16:52:56 -0300

    Commit Inicial

---
+++ src/test/java/br/com/uniesp/terceiroperiodo/TerceiroPeriodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniesp.terceiroperiodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerceiroPeriodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
